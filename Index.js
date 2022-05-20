d1d40f2575da91e96e3f75cdf1bc1eebc47d14b2
CSE3008 <211368@utags.edu.mx>
2022-05-19 21:20:13 -0500

    Create File Firts Add

---
+++ Index.js
@@ -0,0 +1,10 @@
+function doSomething (){
+    console.log ("Hola Mundo Develop");
+}
+
+function develop (){
+    console.log ("I am Develop");
+}
+function Testing (){
+    console.log ("I am Testing");
+}
